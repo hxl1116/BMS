249fda8c394175aa8a0969b945d4e831b4fa7778
Henry Larson <hxl1116@g.rit.edu>
2019-04-14 18:21:54 -0400

    Updated .gitignore to include pom.

--- src/main/java/ControlDeskEvent.java
+++ src/main/java/ControlDeskEvent.java
@@ -2,34 +2,33 @@
  *
  *  Version:
  *  		$Id$
- * 
+ *
  *  Revisions:
  * 		$Log$
- * 
+ *
  */
 
 /**
  * Class that represents control desk event
- *
  */
 
 import java.util.*;
 
 public class ControlDeskEvent {
 
-	/** A representation of the wait queue, containing party names */
-	private Vector partyQueue;
+    /** A representation of the wait queue, containing party names */
+    private Vector partyQueue;
 
     /**
      * Contstructor for the ControlDeskEvent
      *
-     * @param partyQueue	a Vector of Strings containing the names of the parties in the wait queue
+     * @param partyQueue    a Vector of Strings containing the names of the parties in the wait queue
      *
      */
 
-	public ControlDeskEvent( Vector partyQueue ) {
-		this.partyQueue = partyQueue;
-	}
+    public ControlDeskEvent(Vector partyQueue) {
+        this.partyQueue = partyQueue;
+    }
 
     /**
      * Accessor for partyQueue
@@ -39,8 +38,8 @@ public class ControlDeskEvent {
      *
      */
 
-	public Vector getPartyQueue() {
-		return partyQueue;
-	}
+    public Vector getPartyQueue() {
+        return partyQueue;
+    }
 
 }
--- src/main/java/ControlDeskObserver.java
+++ src/main/java/ControlDeskObserver.java
@@ -2,19 +2,18 @@
  *
  *  Version
  *  $Id$
- * 
+ *
  *  Revisions:
  * 		$Log$
- * 
+ *
  */
 
 /**
  * Interface for classes that observe control desk events
- *
  */
 
 public interface ControlDeskObserver {
 
-	public void receiveControlDeskEvent(ControlDeskEvent ce);
+    public void receiveControlDeskEvent(ControlDeskEvent ce);
 
 }
